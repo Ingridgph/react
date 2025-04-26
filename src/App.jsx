4b7fd456bbb8f8be95cdff93e2fd122f99a52d04
Ingridgph <ingridgomespereira98@gmail.com>
2025-04-26 20:00:12 -0300

    wip

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Tasks from './components/Tasks'
+
+function App() {
+  <div>
+  <h1>Gerenciador de tarefas</h1>
+  <Tasks/>
+  </div>
+
+}
+
+export default App
